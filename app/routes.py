bfbe8c564a48772076848edbd13616a216b570a6
EC2 Default User <ec2-user@ip-172-31-22-175.us-east-2.compute.internal>
2019-07-24 13:41:25 +0000

    final version?

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,25 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    user = {"name":"Jeffron", "favGame":"Debugger"}
+    return render_template("index.html", user = user)
+
+@app.route("/secret")
+def secret():
+    return render_template("secret.html")
+    
+@app.route("/howdy")
+def howdy():
+    return render_template("pensiveYeehaw.html")
+    
+@app.route("/sendBreakfast", methods = ["GET", "POST"])
+def sendBreakfast():
+    if request.method == 'GET':
+        return "You didn't fill out the form. I bet you say routes weird"
+    else:
+        userData = dict(request.form)
+        nickname = userData["nickname"][0]
+        breakfast = userData["breakfast"][0]
+
+        
+        return render_template("breakfast.html", nickname = model.shout(nickname), breakfast = model.shout(breakfast) )
